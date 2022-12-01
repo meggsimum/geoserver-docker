376703137042fa971d484e54123756b6bd8506d3
Jakob Miksch <jakob@meggsimum.de>
2022-12-01 16:51:28 +0100

    Check if UI and REST API exists

--- tests/cors.sh
+++ tests/cors.sh
@@ -5,9 +5,11 @@
 # otherwise exit with error code              #
 ###############################################
 
-LINES=$(curl --head -s 'http://localhost:8080/geoserver/ows?service=wfs&version=1.1.0&request=GetCapabilities' | grep -F "Access-Control-Allow-Origin: *" | wc -l)
+echo "Checking if GeoServer supports CORS"
 
-if [ $LINES -eq 1 ]
+LINES=$(curl --head -s 'http://localhost:8080/geoserver/ows?service=wfs&version=1.1.0&request=GetCapabilities' | grep -F -c "Access-Control-Allow-Origin: *")
+
+if [ "${LINES}" -eq 1 ]
 then
   echo "CORS Header exists";
 else
